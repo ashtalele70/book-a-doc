cc3c26fbffd601f6605e246c26ac4d8fbd5b51f0
terrylinda13@gmail.com <terrylinda13@gmail.com>
2021-04-10 11:53:43 -0700

    accepted Ashwini's changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import { IonApp, IonLoading } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import AppTabs from "./AppTabs";
 import { AuthContext, useAuthInit } from "./auth";
-import LoginPage from "./pages/LoginPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import RegisterPage from "./pages/RegisterPage";
-import DoctorProfile from "./pages/DoctorProfile";
 
 const App: React.FC = () => {
   const { loading, auth } = useAuthInit();
@@ -20,21 +15,9 @@ const App: React.FC = () => {
     <IonApp>
       <AuthContext.Provider value={auth}>
         <IonReactRouter>
-          <Switch>
-            <Route exact path="/login">
-              <LoginPage />
-            </Route>
-            <Route exact path="/register">
-              <RegisterPage />
-            </Route>
-            <Route path="/">
-              <AppTabs />
-            </Route>
-            <Redirect exact path="/" to="/home" />
-            <Route>
-              <NotFoundPage />
-            </Route>
-          </Switch>
+          <Route path="/">
+            <AppTabs />
+          </Route>
         </IonReactRouter>
       </AuthContext.Provider>
     </IonApp>
@@ -42,35 +25,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-=======
-import {
-	IonApp, IonLoading,
-  } from '@ionic/react';
-  import { IonReactRouter } from '@ionic/react-router';
-  import React from 'react';
-  import { Route } from 'react-router-dom';
-  import AppTabs from './AppTabs';
-  import { AuthContext, useAuthInit } from './auth';
-  
-  const App: React.FC = () => {
-	const { loading, auth } =  useAuthInit();
-	if (loading) {
-	  return <IonLoading isOpen />;
-	}
-	console.log(`rendering App with auth:`, auth);
-	return (
-	  <IonApp>
-		<AuthContext.Provider value={auth}>
-		  <IonReactRouter>
-			  <Route path="/">
-				<AppTabs />
-			  </Route>
-		  </IonReactRouter>
-		</AuthContext.Provider>
-	  </IonApp>
-	);
-  };
-  
-  export default App;
-  
->>>>>>> bc0e2f5a7f7e1fec00a1d8edde9e20e5bd89a70b
