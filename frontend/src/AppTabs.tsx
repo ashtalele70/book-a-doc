87f632b85e06b4627d5a750ac387efec4e3591ac
terrylinda13@gmail.com <terrylinda13@gmail.com>
2021-04-14 10:44:23 -0700

    Doctor listing page
    This page lists all  doctors returned from firestore. It also allows the user to make appointments and talk to the doctor if the docotr
    is available

--- frontend/src/AppTabs.tsx
+++ frontend/src/AppTabs.tsx
@@ -1,89 +1,100 @@
 import {
-	IonIcon,
-	IonLabel,
-	IonRouterOutlet,
-	IonTabBar,
-	IonTabButton,
-	IonTabs,
-  } from '@ionic/react';
-  import { search as homeIcon, settings as settingsIcon, personCircleOutline, timeOutline } from 'ionicons/icons';
-  import React from 'react';
-  import { Redirect, Route } from 'react-router-dom';
-  import { useAuth } from './auth';
-  import AppointmentsPage from './pages/AppointmentsPage';
-  import HomePage from './pages/HomePage';
-  import LoginDoctorPage from './pages/LoginDoctorPage';
-  import LoginPage from './pages/LoginPage';
-  import LoginSignUpPage from './pages/LoginSignUpPage';
-  import PatientProfilePage from './pages/PatientProfilePage';
-  import DoctorProfilePage from './pages/DoctorProfilePage';
-  import RegisterDoctorPage from './pages/RegisterDoctorPage';
-  import RegisterPage from './pages/RegisterPage';
-  import SettingsPage from './pages/SettingsPage';
-  
-  const AppTabs: React.FC = () => {
-	const { loggedIn } = useAuth();
-	// if (!loggedIn) {
-	//   return <Redirect to="/home" />;
-	// }
-	return (
-	  <IonTabs>
-		<IonRouterOutlet>
-		  <Redirect exact path="/" to="/home" />
-		  <Route exact path="/home">
-			<HomePage />
-		  </Route>
-		  <Route exact path="/settings">
-			<SettingsPage />
-		  </Route>
-		  <Route exact path="/login-signup">
-			<LoginSignUpPage />
-		  </Route>
-		  <Route exact path="/appointments">
-			<AppointmentsPage />
-		  </Route>
-		  <Route exact path="/login">
-			<LoginPage />
-		  </Route>
-		  <Route exact path="/register">
-			<RegisterPage />
-	      </Route>
-		  <Route exact path="/loginDoctor">
-			<LoginDoctorPage />
-		  </Route>
-		  <Route exact path="/registerDoctor">
-			<RegisterDoctorPage />
-	      </Route>
-		  <Route exact path="/patientProfile">
-			<PatientProfilePage />
-	      </Route>
-		  <Route exact path="/doctorProfile">
-			<DoctorProfilePage />
-	      </Route>
-		</IonRouterOutlet>
-		<IonTabBar slot="bottom">
-		  <IonTabButton tab="home" href="/home">
-			<IonIcon icon={homeIcon} />
-			<IonLabel>Search</IonLabel>
-		  </IonTabButton>
-		  
-		  
-		  <IonTabButton tab="appointments" href="/appointments">
-			<IonIcon icon={timeOutline} />
-			<IonLabel>Appointments</IonLabel>
-		  </IonTabButton>
-		  { !loggedIn && <IonTabButton tab="login-signup" href="/login-signup">
-			<IonIcon icon={personCircleOutline} />
-			<IonLabel>Sign Up/ Login</IonLabel>
-		  </IonTabButton>}
-		  { loggedIn && <IonTabButton tab="settings" href="/settings">
-			<IonIcon icon={settingsIcon} />
-			<IonLabel>Settings</IonLabel>
-		  </IonTabButton>}
-		</IonTabBar>
-	  </IonTabs>
-	);
-  };
-  
-  export default AppTabs;
-  
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from "@ionic/react";
+import {
+  search as homeIcon,
+  settings as settingsIcon,
+  personCircleOutline,
+  timeOutline,
+} from "ionicons/icons";
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { useAuth } from "./auth";
+import AppointmentsPage from "./pages/AppointmentsPage";
+import HomePage from "./pages/HomePage";
+import LoginDoctorPage from "./pages/LoginDoctorPage";
+import LoginPage from "./pages/LoginPage";
+import LoginSignUpPage from "./pages/LoginSignUpPage";
+import PatientProfilePage from "./pages/PatientProfilePage";
+import DoctorProfilePage from "./pages/DoctorProfilePage";
+import RegisterDoctorPage from "./pages/RegisterDoctorPage";
+import RegisterPage from "./pages/RegisterPage";
+import SettingsPage from "./pages/SettingsPage";
+import Doctors from "./pages/Doctors";
+
+const AppTabs: React.FC = () => {
+  const { loggedIn } = useAuth();
+  // if (!loggedIn) {
+  //   return <Redirect to="/home" />;
+  // }
+  return (
+    <IonTabs>
+      <IonRouterOutlet>
+        <Redirect exact path="/" to="/home" />
+        <Route exact path="/home">
+          <HomePage />
+        </Route>
+        <Route exact path="/settings">
+          <SettingsPage />
+        </Route>
+        <Route exact path="/login-signup">
+          <LoginSignUpPage />
+        </Route>
+        <Route exact path="/appointments">
+          <AppointmentsPage />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/register">
+          <RegisterPage />
+        </Route>
+        <Route exact path="/loginDoctor">
+          <LoginDoctorPage />
+        </Route>
+        <Route exact path="/registerDoctor">
+          <RegisterDoctorPage />
+        </Route>
+        <Route exact path="/patientProfile">
+          <PatientProfilePage />
+        </Route>
+        <Route exact path="/doctorProfile">
+          <DoctorProfilePage />
+        </Route>
+        <Route exact path="/doctors">
+          <Doctors />
+        </Route>
+      </IonRouterOutlet>
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="home" href="/home">
+          <IonIcon icon={homeIcon} />
+          <IonLabel>Search</IonLabel>
+        </IonTabButton>
+
+        <IonTabButton tab="appointments" href="/appointments">
+          <IonIcon icon={timeOutline} />
+          <IonLabel>Appointments</IonLabel>
+        </IonTabButton>
+        {!loggedIn && (
+          <IonTabButton tab="login-signup" href="/login-signup">
+            <IonIcon icon={personCircleOutline} />
+            <IonLabel>Sign Up/ Login</IonLabel>
+          </IonTabButton>
+        )}
+        {loggedIn && (
+          <IonTabButton tab="settings" href="/settings">
+            <IonIcon icon={settingsIcon} />
+            <IonLabel>Settings</IonLabel>
+          </IonTabButton>
+        )}
+      </IonTabBar>
+    </IonTabs>
+  );
+};
+
+export default AppTabs;
--- frontend/src/pages/Doctors.tsx
+++ frontend/src/pages/Doctors.tsx
@@ -4,6 +4,12 @@ import Rating from "react-rating";
 import starempty from "./images/star-empty.png";
 import starfull from "./images/star-full.png";
 import { firestore } from "../firebase";
+import moment from "moment";
+import {
+  heart,
+  chevronBackOutline as back,
+  chevronForwardOutline as front,
+} from "ionicons/icons";
 import {
   IonButton,
   IonIcon,
@@ -28,22 +34,137 @@ import {
 import "./styleSheet.css";
 
 const Doctors: React.FC = () => {
-  const [entry, setEntry] = useState<any>();
+  const [entry, setEntry] = useState([]);
+  const [timeSlot, settimeSlot] = useState([]);
+  const [showMore, setshowMore] = useState(false);
+  let startDate = new Date();
   useEffect(() => {
     firestore
       .collection("doctors")
-      .where("doctorId", "==", "1")
       .get()
-      .then((snapshot) => {
-        const entry = snapshot.docs[0].data();
-        //const entry = 1;
-        console.log(entry);
-        setEntry(entry);
-        console.log(entry.doctorName);
-        // doc.data() is never undefined for query doc snapshots
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+          let doctorInfo = {
+            id: doc.id,
+            info: doc.data(),
+          };
+          console.log(typeof entry);
+          setEntry((entry) => [...entry, doctorInfo]);
+
+          let timeSlotInfo = [];
+          doc.ref
+            .collection("timeslots")
+            .get()
+            .then((innerQuerySnapshot) => {
+              innerQuerySnapshot.forEach((timeslot) => {
+                // doc.data() is never undefined for query doc snapshots
+                //console.log(doc.id, " => ", timeslot.data());
+                let timehhmm = timeslot.data().time.split(":");
+                var d = new Date();
+                d.setHours(timehhmm[0], timehhmm[1], 0, 0);
+                console.log(d);
+                /*
+                if (d > new Date()) {
+                 
+                }*/
+                timeSlotInfo.push(d);
+                console.log(timeSlotInfo);
+              });
+              timeSlotInfo.sort();
+              settimeSlot((timeSlot) => [...timeSlot, timeSlotInfo]);
+            });
+        });
       });
   }, []);
 
+  function daysOfWeek() {
+    let dates = [];
+    let i;
+    console.log(Object.values(timeSlot));
+    for (i = 1; i <= 7; i++) {
+      startDate.setDate(startDate.getDate() + 1);
+      dates.push(
+        startDate.toLocaleString("default", {
+          weekday: "short",
+          month: "short",
+          day: "numeric",
+        })
+      );
+    }
+
+    return dates;
+  }
+  function handleClick() {
+    setshowMore(!showMore);
+    var btnText = document.getElementById("myBtn");
+    btnText.innerHTML = showMore ? "Read More" : "Read Less";
+  }
+  function getRows() {
+    let table = [];
+    console.log("called");
+
+    timeSlot &&
+      Object.keys(timeSlot).map((key, value) => {
+        let i;
+        const numberOfItems = showMore ? timeSlot[key].length : 5;
+        for (i = 1; i <= numberOfItems; i++) {
+          let j;
+          let children = [];
+          for (j = 1; j <= 7; j++) {
+            children.push(
+              <td>
+                <IonButton color="warning">
+                  {timeSlot[key][j].toLocaleTimeString([], {
+                    timeStyle: "short",
+                  })}
+                </IonButton>
+              </td>
+            );
+          }
+
+          table.push(<tr>{children}</tr>);
+          <button onClick={() => handleClick()}>Show more</button>;
+        }
+      });
+    console.log(table);
+    return table;
+  }
+
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var btnText = document.getElementById("myBtn");
+    var moreText = document.getElementById("more");
+
+    if ((btnText.innerHTML = "Read more")) {
+      dots.style.display = "inline";
+
+      moreText.style.display = "none";
+    } else {
+      btnText.innerHTML = "Read less";
+      dots.style.display = "none";
+      moreText.style.display = "inline";
+    }
+  }
+
+  const list = Object.keys(entry).map((key, value) => {
+    return (
+      <IonItem>
+        <div className="row">
+          Dr. {entry[key].info.firstname} {entry[key].info.lastname}
+          <IonButton color="warning">Talk now</IonButton>
+        </div>
+
+        <div className="row">
+          <table>{getRows()}</table>
+          <IonButton onClick={() => handleClick()} id="myBtn">
+            Read more
+          </IonButton>
+        </div>
+      </IonItem>
+    );
+  });
   return (
     <IonContent
       scrollEvents={true}
@@ -54,6 +175,34 @@ const Doctors: React.FC = () => {
       <IonItemDivider color="primary">
         <IonLabel></IonLabel>
       </IonItemDivider>
+      <IonItem>
+        <div className="row">25 Entries</div>
+        <div className="row">
+          <IonGrid>
+            <IonCol>
+              <IonCol>
+                <IonIcon icon={back}></IonIcon>
+              </IonCol>
+              <table>
+                <tr>
+                  {daysOfWeek().map(function (name, index) {
+                    return (
+                      <td>
+                        <IonCol>{name.split(",")[0]}</IonCol>
+                      </td>
+                    );
+                  })}
+                </tr>
+              </table>
+
+              <IonCol>
+                <IonIcon icon={front}></IonIcon>
+              </IonCol>
+            </IonCol>
+          </IonGrid>
+        </div>
+      </IonItem>
+      {list}
     </IonContent>
   );
 };
