9af257e91622baa5c65a21a9441d81213da86d3b
ashtalele70 <ashtalele70@gmail.com>
2021-05-04 18:05:05 -0500

    datalist issue

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -146,12 +146,19 @@ const Admin: React.FC = (): any => {
                 <details>
                   <summary>show</summary>
 
-                  <datalist>
+                  {/* <datalist>
                     {entry[key].info &&
                       entry[key].info.specialties.map((row, index) => (
                         <option>{row} </option>
                       ))}
-                  </datalist>
+                  </datalist> */}
+
+				  <ul>
+                    {entry[key].info &&
+                      entry[key].info.specialties.map((row, index) => (
+                        <li>{row} </li>
+                      ))}
+                  </ul>
                 </details>
               </IonCol>
             </IonRow>
@@ -164,12 +171,18 @@ const Admin: React.FC = (): any => {
                 <details>
                   <summary>show</summary>
 
-                  <datalist>
+                  {/* <datalist>
                     {entry[key].info &&
                       entry[key].info.educations.map((row, index) => (
                         <option>{row} </option>
                       ))}
-                  </datalist>
+                  </datalist> */}
+				   <ul>
+                    {entry[key].info &&
+                      entry[key].info.educations.map((row, index) => (
+                        <li>{row} </li>
+                      ))}
+                  </ul>
                 </details>
               </IonCol>
             </IonRow>
@@ -199,6 +212,7 @@ const Admin: React.FC = (): any => {
     <IonPage>
       <IonHeader>
         <h1 style={{ color: "#2dd36f", fontWeight: 600 }}>{t('Book-a-Doc')}</h1>
+		<caption style={{ color: "#2dd36f", fontWeight: 400, width: 200 }}><h2>Doctors List</h2></caption>
         <IonTitle color="primary">{entry && entry.length} {t('results')}</IonTitle>
         <IonRadioGroup value={language} onIonChange={e => handleLanguageChange(e.detail.value)}>
                 <IonRow>
